3bd1e434ba88d8ef1a611f58ada4337ff4aac243
LucianCov <lucianc@mit.edu>
2022-02-10 22:03:31 +0000

    made the custom message

--- open_space_publisher.py
+++ open_space_publisher.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 
-from cmath import pi
-from operator import indexOf
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32
-import numpy as np
+from ros_exercises.msg import OpenSpace
+from cmath import pi
+from operator import indexOf
 
 
 def callback(data):
     dist = max(data.ranges)
     ang = data.angle_min + indexOf(data.ranges, dist)*data.angle_increment
+    combined = OpenSpace()
+    combined.angle = ang
+    combined.distance = dist
     dist_pub = rospy.Publisher('open_space/distance', Float32, queue_size=10)
     ang_pub = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+    custom_pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
 
     dist_pub.publish(dist)
     ang_pub.publish(ang)
+    custom_pub.publish(combined)
 
 
 def listen():
